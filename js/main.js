a3dc681792ab1758f6693b6b1061c6fe99c81988
Nadaakn <nadaa.khoirunnafisah@gmail.com>
2024-01-10 23:32:31 +0700

    fix top and banner

--- js/main.js
+++ js/main.js
@@ -17,13 +17,13 @@
 
 
     // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 300) {
+    //         $('.navbar').fadeIn('slow').css('display', 'flex');
+    //     } else {
+    //         $('.navbar').fadeOut('slow').css('display', 'none');
+    //     }
+    // });
 
 
     // Smooth scrolling on the navbar links
